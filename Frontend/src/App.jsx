ff83f70f836c9f97117e117228957c1b575c5249
Akash Dwivedi <akashdwivedi@Akashs-MacBook-Air.local>
2026-03-10 16:04:47 +0530

    Add backend and frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect} from 'react'
+import axios from "axios"
+
+function App() {
+
+  const [notes, setNotes] = useState([
+  {
+    title:"test title 1",
+    description:"test description "
+  },
+  {
+    title:"test title 2",
+    description:"test description "
+  },
+  {
+    title:"test title 3",
+    description:"test description "
+  },
+  {
+    title:"test title 4",
+    description:"test description "
+  }  
+])
+
+  function fetchNotes(){
+      axios.get('http://localhost:3000/api/notes')
+      .then((res)=>{
+      setNotes(res.data.notes)
+    })
+  }
+  
+
+  useEffect(()=>{
+    fetchNotes()
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const {title, description} = e.currentTarget.elements
+
+    console.log(title.value,description.value)
+
+    axios.post("http://localhost:3000/api/notes",{
+      title:title.value,
+      description:description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+
+      fetchNotes()
+    })
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/"+noteId,)
+    .then(res=>{
+      
+      fetchNotes()
+    })
+  }
+
+
+  return (
+    <>
+
+      <form className='note-create-form' onSubmit ={handleSubmit}>
+        <input type="text" name="title"placeholder='Enter title'/>
+        <input type="text" name="description" placeholder='Enter Description'/>
+        <button >Create Note</button>
+
+        
+      </form>
+
+      <div className="notes">
+        {
+          notes.map(note =>{
+            return <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              <button onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
+            </div>
+          })
+        }
+       
+      </div>
+    </>
+  )
+}
+
+export default App
